03c727e5af13a7e33b02a14f8fc439eab5e5ef0e
EllisGL <xellisx@gmail.com>
2026-02-03 14:26:20 -0600

    Removed uneeded lines and spaces.

--- src/1602-I2C-demo.s
+++ src/1602-I2C-demo.s
@@ -6,12 +6,8 @@
 ; Hardware Definitions
 ; ========================================
 VIA_BASE_ADR = $6000
-
-    .global VIA_PB
-    .global VIA_DDRB
-
-VIA_PB   = VIA_BASE_ADR + 0
-VIA_DDRB = VIA_BASE_ADR + 2
+VIA_PB       = VIA_BASE_ADR + 0
+VIA_DDRB     = VIA_BASE_ADR + 2
 
 ; I2C LCD address
 LCD_I2C_ADDR = $27
@@ -24,30 +20,30 @@ LCD_I2C_ADDR = $27
 start:
     ; Initialize I2C
     JSR I2C_Init
-    
+
     ; Initialize LCD at address 0x27
     LDA #LCD_I2C_ADDR
     JSR LCD_Init
-    
+
     ; Write "Hello World!"
     LDA #<msg_hello
     STA LCD_StrPtr
     LDA #>msg_hello
     STA LCD_StrPtr+1
     JSR LCD_WriteString
-    
+
     ; Set cursor to second line (row 1, col 0)
     LDA #0              ; Column 0
     LDX #1              ; Row 1
     JSR LCD_SetCursor
-    
+
     ; Write "6502 + I2C LCD"
     LDA #<msg_6502
     STA LCD_StrPtr
     LDA #>msg_6502
     STA LCD_StrPtr+1
     JSR LCD_WriteString
-    
+
     ; Done - infinite loop
 loop:
     JMP loop
